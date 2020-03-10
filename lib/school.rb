bae173bd11eaaeecd4fa08848c35e4975706ba11
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-10 18:32:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -26,4 +26,7 @@ class School
   end
 end
 
-school1 = School.new("George Jenkins").sort
+school1 = School.new("George Jenkins")
+school1.add_student("Jeremy", 7)
+school1.add_student("David", 7)
+school1.sort
