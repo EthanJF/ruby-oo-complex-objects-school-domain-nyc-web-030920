f8cb38987829cd1d51251d33d6210900ca89dc68
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-10 17:55:26 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,14 @@
 # code here!
 class School
   
+  attr_accessor :name, :roster
+  
   def initialize(name)
     @name = name
     @roster = []
   end
   
-  def add_student(student)
+  def add_student(student, grade)
     @roster << student
   end
   
