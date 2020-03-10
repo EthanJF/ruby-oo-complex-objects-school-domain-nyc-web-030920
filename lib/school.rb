84371e3a7bafdf4dff693aa433d8f7445a78ef35
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-10 17:53:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = []
+  end
+  
+  def add_student(student)
+    @roster << student
+  end
+  
+  def grade
+  end
+  
+  def sort
+  end
+end
